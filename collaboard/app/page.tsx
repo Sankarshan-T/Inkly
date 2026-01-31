c0421c05e30e82a9051c6f08216df9bd565fce66
CoolCream <mahasankarshant@gmail.com>
2026-01-31 14:59:10 +0530

    loading

--- collaboard/app/page.tsx
+++ collaboard/app/page.tsx
@@ -1,9 +1,29 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
+import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size="sm">
-      Example
-    </Button>
+    <>
+      <Authenticated>
+        <div className="flex flex-col gap-y-4">
+            <div>
+              Authenticated!!
+            </div>
+            <div>
+              <UserButton />
+            </div>
+        </div>
+      </Authenticated>
+
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+
+      <AuthLoading>
+        <div>Loading...</div>
+      </AuthLoading>
+    </>
   );
 }

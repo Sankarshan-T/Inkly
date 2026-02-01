8d4dfe2a23b307d5f497918ab7c9fcd66df6021f
CoolCream <mahasankarshant@gmail.com>
2026-02-01 18:03:52 +0530

    mobile responsiveness

--- collaboard/app/(dashboard)/_components/navbar.tsx
+++ collaboard/app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignInButton, UserButton } from "@clerk/nextjs";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { SearchInput } from "./search-input";
 
@@ -9,6 +9,58 @@ export const Navbar = () => {
         <div className="flex items-center gap-x-4 p-5">
             <div className="hidden lg:flex lg:flex-1">
                 <SearchInput />
+            </div>
+            <div className="block lg:hidden flex-1">
+                <OrganizationSwitcher
+                    hidePersonal
+                    appearance={{
+                        elements: {
+                            rootBox: {
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "100%",
+                                maxWidth: "376px",
+                                height: "50px"
+                            },
+                            button: {
+                                width: "100%",
+                                height: "100%",
+                                justifyContent: "space-between",
+                            },
+                            organizationSwitcherTrigger: {
+                                padding: "6px",
+                                width: "100%",
+                                borderRadius: "8px",
+                                border: "1px solid #E5E7EB",
+                                justifyContent: "space-between",
+                                backgroundColor: "white",
+                            },
+                            organizationPreview: {
+                                height: "100%",
+                            },
+                            organizationPreviewAvatarContainer: {
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: 'center',
+                                height: "100%",
+                            },
+                            organizationPreviewAvatarBox: {
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: 'center',
+                                height: "100%",
+                                width: "35px",
+                            },
+                            avatarImage: {
+                                height: "100%",
+                            },
+                            organizationPreviewMainIdentifier: {
+                                fontSize: "1rem",
+                            },
+                        }
+                    }}
+                />
             </div>
             <UserButton />           
         </div>

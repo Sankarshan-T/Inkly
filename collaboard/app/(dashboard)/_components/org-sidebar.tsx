221034b001d1cba7f1e69cac5754a47e49509fb7
CoolCream <mahasankarshant@gmail.com>
2026-02-01 16:37:14 +0530

    sidebarswitcher

--- collaboard/app/(dashboard)/_components/org-sidebar.tsx
+++ collaboard/app/(dashboard)/_components/org-sidebar.tsx
@@ -6,6 +6,9 @@ import { Poppins } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard, Star } from "lucide-react";
+import { useSearchParams } from "next/navigation";
 
 const font = Poppins({
     subsets: ["latin"],
@@ -13,6 +16,9 @@ const font = Poppins({
 });
 
 export const OrgSidebar = () => {
+    const searchParams = useSearchParams();
+    const favorites = searchParams.get("favorites");
+
     return(
         <div className="hidden lg:flex flex-col space-y-6 w-51.5 pl-5 pt-5">
             <Link href='/'>
@@ -80,6 +86,33 @@ export const OrgSidebar = () => {
                     }
                 }}
             />
+            <div className="space-y-1 w-full">
+                <Button
+                    variant={favorites ? "outline" : "secondary"}
+                    asChild
+                    size="lg"
+                    className="font-normal justify-start px-2 w-full h-12.5"
+                >
+                    <Link href="/">
+                        <LayoutDashboard className="h-4 w-4 mr-2" />
+                        Team Boards
+                    </Link>
+                </Button>
+                <Button
+                    variant={favorites ? "secondary" : "outline"}
+                    asChild
+                    size="lg"
+                    className="font-normal justify-start px-2 w-full h-12.5"
+                >
+                    <Link href={{
+                        pathname: "/",
+                        query: { favorites: true }
+                    }}>
+                        <Star className="h-4 w-4 mr-2" />
+                        Favorite Boards
+                    </Link>
+                </Button>
+            </div>
         </div>
     );
 };

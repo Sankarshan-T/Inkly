18513a2a2627163c4cf890acc154bd318d594b8f
CoolCream <mahasankarshant@gmail.com>
2026-02-08 12:50:27 +0530

    board created!

--- collaboard/app/(dashboard)/_components/new-board.tsx
+++ collaboard/app/(dashboard)/_components/new-board.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { toast } from "sonner";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
 
@@ -12,12 +15,27 @@ export const NewBoard = ({
     orgId,
     disabled
     }: NewBoardProps) => {
+    const { mutate, pending } = useApiMutation(api.board.create);
+    
+    const onClick = () => {
+        mutate({
+            orgId,
+            title: "Untitled"
+        })
+        .then((id) => {
+            toast.success("Board Created!");
+        })
+        .catch(() => {
+            toast.error("Failed to create board");
+        })
+    }
+
     return(
         <button
-            disabled={disabled}
-            onClick={() => {}}
+            disabled={pending || disabled}
+            onClick={onClick}
             className={cn(
-                "col-span-1 aspect-100/127 bg-blue-500 rounded-lg hover:bg-blue-700 flex flex-col items-center justify-center py-6", disabled && "opacity-25"
+                "col-span-1 aspect-100/127 bg-blue-500 rounded-lg hover:bg-blue-700 flex flex-col items-center justify-center py-6", (pending || disabled) && "opacity-25"
             )}>
             <div />
             <Plus className="h-15 w-15 stroke-2 text-white"/>

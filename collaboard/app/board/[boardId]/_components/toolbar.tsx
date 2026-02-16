56d4b65cf27acb41429dab9634229e49226c7710
CoolCream <mahasankarshant@gmail.com>
2026-02-16 20:08:06 +0530

    finished toolbar

--- collaboard/app/board/[boardId]/_components/toolbar.tsx
+++ collaboard/app/board/[boardId]/_components/toolbar.tsx
@@ -1,11 +1,51 @@
+import { Circle, MousePointer2, Pencil, Square, StickyNote, Type } from "lucide-react";
+import { ToolButton } from "./tool-button";
+
 export const Toolbar = () => {
-    return(
+    return (
         <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                <div>Pencil</div>
-                <div>Square</div>
-                <div>Rectangle</div>
-                <div>Marker</div>
+                <ToolButton
+                    label="Select"
+                    icon={MousePointer2}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+
+                <ToolButton
+                    label="Text"
+                    icon={Type}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+
+                <ToolButton
+                    label="Sticky Note"
+                    icon={StickyNote}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+
+                <ToolButton
+                    label="Rectagle"
+                    icon={Square}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+
+                <ToolButton
+                    label="Ellipse"
+                    icon={Circle}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+
+                <ToolButton
+                    label="Pen"
+                    icon={Pencil}
+                    onClick={() => { }}
+                    isActive={false}
+                />
             </div>
             <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
                 <div>Undo</div>
@@ -16,8 +56,8 @@ export const Toolbar = () => {
 };
 
 export const ToolbarSkeleton = () => {
-    return(
-        <div 
+    return (
+        <div
             className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-90 w-13 shadow-md rounded-md"
         />
     )

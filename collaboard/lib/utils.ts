1e915a529fc9fa839953d812a078a497d7d94ab1
CoolCream <mahasankarshant@gmail.com>
2026-02-16 19:53:41 +0530

    border-colors

--- collaboard/lib/utils.ts
+++ collaboard/lib/utils.ts
@@ -1,6 +1,21 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "#08081c",
+  "#141347",
+  "#2bc2ad",
+  "#d43d52",
+  "#d50e71",
+  "#732bde",
+  "#12a66b",
+  "#d47118",
+]
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+};
